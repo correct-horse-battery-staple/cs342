20b85ce56fb612dd15ffe519afa1b484892946c8
correct-horse-battery-staple <pandasmoothie@gmail.com>
2017-02-24 15:12:22 -0500

    Created new input fragment for creating markers, with associated layout.
    In progress:
    onClick functionality
    adding fragment to activity
    testing ability to add markers

---
+++ MyAmherstKevin/app/src/main/java/com/example/myamherstkevin/InputFragment.java
@@ -0,0 +1,11 @@
+package com.example.myamherstkevin;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by panda_000 on 2/24/2017.
+ */
+
+public class InputFragment extends Fragment {
+
+}

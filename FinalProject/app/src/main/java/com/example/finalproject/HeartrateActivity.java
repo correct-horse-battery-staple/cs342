70e25571491690ddb34a530ed9fba8c09bc4b241
correct-horse-battery-staple <pandasmoothie@gmail.com>
2017-04-17 16:13:47 -0400

    added interface for home screen and imported the graphview .jar

---
+++ FinalProject/app/src/main/java/com/example/finalproject/HeartrateActivity.java
@@ -0,0 +1,14 @@
+
+package com.example.finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HeartrateActivity extends ServerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_heartrate);
+    }
+}
---
+++ FinalProject/app/src/main/java/com/example/finalproject/WeightActivity.java
@@ -0,0 +1,13 @@
+package com.example.finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WeightActivity extends ServerActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weight);
+    }
+}

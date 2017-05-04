2c2a2a06cf97e1a6fa4cb658ec470791ffe586e9
correct-horse-battery-staple <pandasmoothie@gmail.com>
2017-05-04 00:20:47 -0400

    weight graphs now display, though not with date-time; will work on in future

--- FinalProject/app/src/main/java/com/example/finalproject/WeightActivity.java
+++ FinalProject/app/src/main/java/com/example/finalproject/WeightActivity.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 
 import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
 import com.jjoe64.graphview.LabelFormatter;
 import com.jjoe64.graphview.Viewport;
 import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
@@ -24,6 +25,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -33,29 +35,40 @@ public class WeightActivity extends ServerActivity {
     private boolean LBorKG;
     private GraphView graph;
     private BarGraphSeries<DataPoint> dataSeries;
-    private int graphCount = 1;
+    private int graphCount = 8;
+    private int pickerValueLB = 140;
+    private int pickerValueKG = (int)(pickerValueLB/2.20462);
+    private SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weight);
 
+        LBorKG=true;
+
         picker = (NumberPicker)findViewById(R.id.weight_PICKER);
         picker.setMinValue(0);
         picker.setMaxValue(700);
-        picker.setValue(140);
+        picker.setValue(LBorKG?pickerValueLB:pickerValueKG);
 
-        LBorKG=true;
         graph = (GraphView)findViewById(R.id.weight_GRAPH);
         dataSeries = new BarGraphSeries<>();
-        graph.getViewport().setXAxisBoundsManual(true);
-        graph.getViewport().setMinX(0);
-        graph.getViewport().setMaxX(40);
+        dataSeries.setSpacing(15);
+
+        Viewport v = graph.getViewport();
+        v.setXAxisBoundsManual(true);
+        v.setMinX(0);
+        v.setMaxX(40);
+        v.setYAxisBoundsManual(true);
+        v.setMinY(0);
+        v.setMaxY(160);
         graph.addSeries(dataSeries);
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
-        graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter(this,dateFormat));
-        graph.getGridLabelRenderer().setNumHorizontalLabels(graphCount);
+        GridLabelRenderer g = graph.getGridLabelRenderer();
+        //g.setLabelFormatter(new DateAsXAxisLabelFormatter(this,dateFormat));
+        g.setNumHorizontalLabels(2);
+        g.setNumVerticalLabels(7);
         graph.getGridLabelRenderer().setHumanRounding(false);
 
         getData("weight");
@@ -64,6 +77,16 @@ public class WeightActivity extends ServerActivity {
         toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 LBorKG = !LBorKG;
+                int current = picker.getValue();
+                if(LBorKG) {
+                    pickerValueKG = current;
+                    pickerValueLB = (int)(pickerValueKG*2.20462);
+                }
+                else {
+                    pickerValueLB = current;
+                    pickerValueKG = (int)(pickerValueLB/2.20462);
+                }
+                picker.setValue(LBorKG?pickerValueLB:pickerValueKG);
             }
         });
     }
@@ -91,24 +114,44 @@ public class WeightActivity extends ServerActivity {
             try {
                 JSONArray jsonArray = new JSONArray(data.substring(op.length()+1));
                 DataPoint[] points = new DataPoint[jsonArray.length()];
-                long minX= 0;
-                long maxX =70;
+                Date minX = new Date();
+                Date maxX = new Date();
                 for (int i =0;i<jsonArray.length();i++){
                     JSONObject object = (JSONObject)jsonArray.get(i);
                     int value = (Integer)(object.get("value"));
                     String unit = (String)object.get("unit");
                     long datetime = Long.parseLong((String)object.get("datetime"));
                     //Log.d("app load",value+" "+unit+" "+datetime);
-                    points[i]=new DataPoint(datetime,(unit.equals("kg")?value*2.20462:value));
+                    Date d = new Date();
+                    try {
+                        d = dateFormat.parse("" + datetime);
+                    }
+                    catch(ParseException p){
+                        p.printStackTrace();
+                    }
+                    points[i]=new DataPoint(i+1,(unit.equals("kg")?value*2.20462:value));
                     if(i==jsonArray.length()-graphCount-1)
-                        minX = datetime;
+                        minX = d;
                     if(i==jsonArray.length()-1)
-                        maxX = datetime;
-                    Log.d("app load",points[i].toString());
+                        maxX = d;
+                    //Log.d("app load",points[i].toString());
                 }
                 dataSeries.resetData(points);
-                graph.getViewport().setMinX(minX);
-                graph.getViewport().setMaxX(maxX);
+//                graph.getViewport().setMinX(minX);
+//                graph.getViewport().setMaxX(maxX);
+
+//                JSONObject object = (JSONObject)jsonArray.get(jsonArray.length()-1);
+//                int value = (Integer)(object.get("value"));
+//                String unit = (String)object.get("unit");
+//                long datetime = Long.parseLong((String)object.get("datetime"));
+//                Date d = new Date();
+//                try {
+//                    d = dateFormat.parse("" + datetime);
+//                }
+//                catch(ParseException p){
+//                    p.printStackTrace();
+//                }
+//                dataSeries.resetData(new DataPoint[]{new DataPoint(d,(unit.equals("kg")?value*2.20462:value))});
                 //setErrorMessage(data);
                 Log.d("server load",data.substring(op.length()+1));
             }
